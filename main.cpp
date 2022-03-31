027402fd438f337f7a28677b3c04d2593ea3aa94
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-03-31 23:21:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
+       float a =0, b=0, risultato=0;
+    int opzione=0;
+    cout << "Inserire due numeri razionali" << endl;
+    cin >> a >> b;
+    cout << "Inserire opzione" << endl;
+    cin >> opzione;
+    switch (opzione) {
+        case 0:
+            risultato = (a*b)/2;
+            break;
+        case 1:
+            risultato = a*a;
+            break;
+        case 2:
+            risultato = a*b;
+            break;
+        default:
+            cout << "opzione non valida" << endl;
+    }
+cout << risultato << endl;
    return 0;
 }
